7666f459d770355dbfc94460c83ee6596b954eee
Jason Elbourne <jayelbourne@gmail.com>
2018-10-14 00:31:50 -0400

    Initial code for the Project mvc

---
+++ JayElbourne/Controllers/ProjectController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using JayElbourne.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JayElbourne.Controllers
+{
+    [Route("project")]
+    public class ProjectController : Controller
+    {
+        [Route("{slug}")]
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+        [HttpGet, Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("create")]
+        public IActionResult Create(Project project)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            project.Posted = DateTime.Now;
+
+            return View();
+        }
+    }
+}
---
+++ JayElbourne/Models/Project.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace JayElbourne.Models
+{
+    public class Project
+    {
+        public Project()
+        {
+            Featured = false;
+        }
+
+        public long Id { get; set; }
+
+        private string _slug;
+
+        public string Slug
+        {
+            get
+            {
+                if (_slug == null && Title != null)
+                {
+                    _slug = Regex.Replace(Title.ToLower(), "[^a-z0-9]", "-");
+                }
+                return _slug;
+            }
+            set { _slug = value; }
+        }
+
+        [Display(Name = "Project Title")]
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(250, MinimumLength = 5,
+            ErrorMessage = "Title must be between 5 and 100 characters long")]
+        public string Title { get; set; }
+
+        [Required]
+        [MinLength(100, ErrorMessage = "Blog posts must be at least 100 characters long")]
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+
+        public bool Featured { get; set; }
+
+        public DateTime Posted { get; set; }
+
+        public enum Status
+        {
+            [Display(Name = "Work In Progress")]
+            WorkInProgress,
+            Complete
+        }
+    }
+}
